d10f6f845ff964233ddce4dc75d8d5119e7acdfa
marcohidalgo2385 <marcohidalgo2385@gmail.com>
2024-05-21 17:08:35 -0500

    genera gráficos y archivo de streamlit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # proyecto_sprint_5
 Proyecto Final del Sprint 5 del Bootcamp de TripleTen
+
+Esta aplicación permite generar histogramas y gráficos de dispersión para la compra de vehículos
--- app.py
+++ app.py
@@ -1,3 +1,37 @@
 import streamlit as st
 import pandas as pd
-import plotly_express as plt
+import plotly_express as px
+
+car_data = pd.read_csv("vehicles_us.csv")  # leer los datos
+
+st.header('Aplicación Web Venta de Vehículos')
+
+hist_button = st.button('Construir histograma')  # crear un botón
+
+if hist_button:  # al hacer clic en el botón
+    # escribir un mensaje
+    st.write(
+        'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+
+    # crear un histograma
+    fig = px.histogram(car_data, x="odometer")
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+
+scatter_button = st.button(
+    'Construir diagrama de dispersión')  # crear un botón
+
+if scatter_button:  # al hacer clic en el botón
+    # escribir un mensaje
+    st.write('Creación de un diagrama de dispersión para el conjunto de datos de anuncios de venta de coches')
+
+    # crear un diagrama de dispersión
+    # ajusta las columnas según tus necesidades
+    fig = px.scatter(car_data, x="odometer", y="price")
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+
